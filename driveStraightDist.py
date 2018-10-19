4b0fbcde95bbe860a1a81cf1e43f0086917e0362
escime <44032434+escime@users.noreply.github.com>
2018-10-19 11:00:59 -0400

    v0.4
    Added the ability to drive backwards when a negative distance is input by the user.

--- driveStraightDist.py
+++ driveStraightDist.py
@@ -5,8 +5,8 @@ BP = brickpi3.BrickPi3() #Create an instance of the brickpi to be controlled
 
 def straightDrive(distance, speed):
     distanceEncoder = cl.convertEncoder(distance)
+    print(distanceEncoder)
     speedCon = cl.convertSpeed(speed)
-    speedCon = speedCon*1.1
 
     BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A))
     BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
@@ -14,19 +14,36 @@ def straightDrive(distance, speed):
     BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D))
 
     try:
-        while BP.get_motor_encoder(BP.PORT_A) < distanceEncoder:
-            differential = BP.get_motor_encoder(BP.PORT_A) - BP.get_motor_encoder(BP.PORT_D)
-            BP.set_motor_power(BP.PORT_A+BP.PORT_B, speedCon)
-            if(differential >= 3):
-                while differential >= 1:
-                    differential = BP.get_motor_encoder(BP.PORT_A) - BP.get_motor_encoder(BP.PORT_D)
-                    BP.set_motor_power(BP.PORT_A, speedCon*0.7)
-                    BP.set_motor_power(BP.PORT_D, speedCon)
-            if(differential <= -3):
-                while differential <= -1:
-                    differential = BP.get_motor_encoder(BP.PORT_A) - BP.get_motor_encoder(BP.PORT_D)
-                    BP.set_motor_power(BP.PORT_D, speedCon*0.7)
-                    BP.set_motor_power(BP.PORT_A, speedCon)
+        if(distanceEncoder > 0):
+            while BP.get_motor_encoder(BP.PORT_A) < distanceEncoder:
+                differential = BP.get_motor_encoder(BP.PORT_A) - BP.get_motor_encoder(BP.PORT_D)
+                BP.set_motor_power(BP.PORT_A+BP.PORT_B, speedCon)
+                if(differential >= 2):
+                    while differential >= 1:
+                        differential = BP.get_motor_encoder(BP.PORT_A) - BP.get_motor_encoder(BP.PORT_D)
+                        BP.set_motor_power(BP.PORT_A, speedCon*0.7)
+                        BP.set_motor_power(BP.PORT_D, speedCon)
+                if(differential <= -2):
+                    while differential <= -1:
+                        differential = BP.get_motor_encoder(BP.PORT_A) - BP.get_motor_encoder(BP.PORT_D)
+                        BP.set_motor_power(BP.PORT_D, speedCon*0.7)
+                        BP.set_motor_power(BP.PORT_A, speedCon)
+        if(distanceEncoder < 0):
+            speedCon = -1 * speedCon
+            while BP.get_motor_encoder(BP.PORT_A) > distanceEncoder:
+                differential = abs(BP.get_motor_encoder(BP.PORT_A)) - abs(BP.get_motor_encoder(BP.PORT_D))
+                BP.set_motor_power(BP.PORT_A+BP.PORT_B, speedCon)
+                if(differential >= 2):
+                    while differential >= 1:
+                        differential = abs(BP.get_motor_encoder(BP.PORT_A)) - abs(BP.get_motor_encoder(BP.PORT_D))
+                        BP.set_motor_power(BP.PORT_A, speedCon*0.7)
+                        BP.set_motor_power(BP.PORT_D, speedCon)
+                if(differential <= -2):
+                    while differential <= -1:
+                        differential = abs(BP.get_motor_encoder(BP.PORT_A)) - abs(BP.get_motor_encoder(BP.PORT_D))
+                        BP.set_motor_power(BP.PORT_D, speedCon*0.7)
+                        BP.set_motor_power(BP.PORT_A, speedCon)
+
     except IOError as error:
         print(error)
     except TypeError as error:
